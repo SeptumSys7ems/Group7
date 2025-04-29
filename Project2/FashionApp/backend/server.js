05cd158e04a1fdf940b724d1010de7b344b830d6
praveen <cardinalgracias31@gmail.com>
2025-04-28 19:37:34 -0700

    updated app.yaml for backend

--- Project2/FashionApp/backend/server.js
+++ Project2/FashionApp/backend/server.js
@@ -47,11 +47,11 @@ app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uploadsDir = path.join(__dirname, 'uploads');
-const fs = require('fs');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
-}
+// const uploadsDir = path.join(__dirname, 'uploads');
+// const fs = require('fs');
+// if (!fs.existsSync(uploadsDir)) {
+//     fs.mkdirSync(uploadsDir, { recursive: true });
+// }
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
